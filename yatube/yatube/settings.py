2c680f4d82d32d88c225600cbec597bd38ba0ddb
Maria Bykova <mariab@bk.ru>
2022-10-06 09:20:18 +0300

    Добавлен django-debug-toolbar

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     'core.apps.CoreConfig',
     'about.apps.AboutConfig',
     'sorl.thumbnail',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -43,6 +44,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'yatube.urls'
@@ -98,6 +100,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+INTERNAL_IPS = [
+    '127.0.0.1',
+] 
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
