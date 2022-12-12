d36c566ff02fd889e7e31a2b4082940c7d234d13
Maria Bykova <mariab@bk.ru>
2022-12-12 21:05:19 +0300

    test

--- yatube/posts/utils.py
+++ yatube/posts/utils.py
@@ -2,6 +2,8 @@ from django.core.paginator import Paginator
 
 
 def paginate(request, queryset, posts_per_page):
+    """Паджинатор.
+    """
     paginator = Paginator(queryset, posts_per_page)
     page_number = request.GET.get('page')
     return paginator.get_page(page_number)
