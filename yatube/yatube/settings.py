9e56734326e149125aa2e1c1afb94deb7e062192
Maria Bykova <mariab@bk.ru>
2023-02-20 17:05:39 +0300

    Убран debug-toolbar

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'core.apps.CoreConfig',
     'about.apps.AboutConfig',
     'sorl.thumbnail',
-    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -47,7 +46,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'yatube.urls'
